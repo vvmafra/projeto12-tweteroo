2d4896207ada37fb106a5ad3515db8382a608662
Victor Mafra <victorvmafra97@gmail.com>
2023-04-06 12:09:33 -0300

    vpost-sign-up

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
+
 app.use(cors())
+app.use(express.json())
 
-app.get("", (req, res) => {
+const users = []
+const tweets = []
 
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body
 
-    res.send()
+    const newUsers = { username, avatar }
+
+    users.push(newUsers)
+    res.send("OK")
 })
+
+// app.get("", (req, res) => {
+//     res.send()
+// })
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
